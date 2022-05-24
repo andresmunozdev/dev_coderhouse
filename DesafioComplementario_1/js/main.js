73c73e89a47a72211fd4a7d7f64227daad95e9d3
andresmunozdev <andres.munozcat@correoaiep.cl>
2022-05-24 19:14:23 -0400

    actualiza comentarios

--- DesafioComplementario_1/js/main.js
+++ DesafioComplementario_1/js/main.js
@@ -1,8 +1,18 @@
 
 /* 
-Entrega desafío Complementario 
+
+Alumno: Andrés Muñoz C.
+Comisión 31650 
+Tutor: Luis Salinas  
+
+Entrega desafío Complementario - CoderHouse
+
 Desafío: Crear un algoritmo utilizando un ciclo
-*/ 
+Se realiza algoritmo considerando que la escala de notas en Chile es de 1,0 a 7,0
+
+Para efectos de prueba se deja alumno con nota menor a 4,0 reprobado, entre 4,0 y 4,9 Aprobado y puede dar examen de 
+recuperación (opcional) para subir nota y sobre 5,0 queda aprobado.
+*/
 
 
 let cantNotas = parseInt(prompt ("Ingrese Cantidad de Notas a Promediar: "))
@@ -20,7 +30,7 @@ console.log("El Promedio del Alumno es: ",promedio)
 if ( promedio < 4.0 ) {
     console.error("Condición Academica: Alumno Reprobado")
 } else if (promedio >=4.0 && promedio <=4.9 ){ 
-    console.log ("Condición Academica: Alumno Aprobado, Puede Rendir Prueba de Recuperación" )
+    console.log ("Condición Academica: Alumno Aprobado, Puede Rendir Prueba de Recuperación (opcional)" )
 } else if (promedio >=5.0){ 
     console.log ("Condición Academica: Alumno Aprobado, OK" )
 }
